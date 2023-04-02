a6c4eb56504c802bd3b869cb8e8583d38d0492bb
karthik-5480 <95642762+karthik-5480@users.noreply.github.com>
2023-04-02 10:58:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pickle
+
+pickle_in = open("regressor.pkl","rb")
+regressor=pickle.load(pickle_in)
+
+st.title("Salary prediction")
+
+a = float(st.number_input("Experience in years"))
+
+
+
+btn = st.button("predict")
+
+if btn:
+    prediction=regressor.predict([[a]])
+    st.subheader(prediction)
